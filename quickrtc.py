43ac560ca41999ba32b149ac29f67a18636d905b
Rix <rixwoodling@gmail.com>
2022-10-07 16:59:38 -0700

    Update quickrtc.py

--- quickrtc.py
+++ quickrtc.py
@@ -21,11 +21,10 @@ def timestamp():
 
 
 filename = timestamp()[0] + '-webrtc_parse-' + timestamp()[1] + '.csv'
-
-f = open( sys.argv[1], 'r' )
 with open( filename, 'w' ) as file:
     pass
 
+f = open( sys.argv[1], 'r' )
 jdata = json.loads( f.read() )
 k2 = list( jdata['PeerConnections'].keys())
 
@@ -40,36 +39,49 @@ for i in k2:
 # ----->| for each item in row
         for z in range( 0, len( y ) - 1 ):
 
-# --------->| if items are strings, strip quotes and append once
+# --------->| if items are strings, clean up and append first value
             if '"' in y[z]:
-                e = str( y[z].strip( '[\"' ))
+                e = str( y[z] )
+                e = e.strip( '\"' )
+                e = e.strip( '[\"' )
                 if ' ' in e:
                     e = str( e.split( ' ' )[0] )
                 if len( e ) > 20:
                     e = e[:20]
                 v.append( e )
                 break
-# --------->| if list items are int or float, format
+
+# --------->| if list items are int or float, format floats
             else:
+                try: e = format( float( y[z + 1] ), ".1f" )
+                except: pass
+
+                try: e = int( y[z + 1] )
+                except: pass
+
+                v.append( str( e ))
+
+# ----->| average values into second column
+
+        if len( v ) > 3:
+            total = 0
+            average = len( v[1:] )
+            for x in v[1:]:
                 try:
-                    e = format( float( y[z + 1] ), ".1f" )
-                    v.append( str( e ))
+                    total = total + float( x )
                 except:
                     pass
+            avg = format( float( total / average ), ".1f" )
+            v.insert( 1,str( avg ))
 
-                try:
-                    e = int( y[z + 1] )
-                    v.append( str( e ))
-                except:
-                    pass
-
-# ----->|
+# ----->| convert list to csv for write and print
         if len( v ) > 1:
             v2csv = ','.join( v )
             print( v2csv )
             with open( filename,'a' ) as file:
                 file.write( v2csv + "\n" )
             file.close()
+# ----->| do not write if no values in row
         if len( v ) < 2:
             pass
         v = []
